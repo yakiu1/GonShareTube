4de216a55639769b42e5c25b8a403a271816689e
l8iunix8 <l8iunix8@gmail.com>
2020-11-01 21:20:38 +0800

    Kevin/gon list (#9)
    * Kevin/GonList_UI
    
    新增GonList元件 只有UI介面
    
    * Revert "Merge branch 'Kevin/Gon-List' of https://github.com/l8iunix8/GonShareTube into Kevin/Gon-List"
    
    This reverts commit b8ca888ba5fb6b406f89e1b56bb740751cf0ea15, reversing
    changes made to ac60d73d5dc0b499550e5cf61c93f4e6dca4e5ca.

---
+++ src/app/shared/components/gon-list/gon-list.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { GonListComponent } from './gon-list.component';
+
+describe('GonListComponent', () => {
+  let component: GonListComponent;
+  let fixture: ComponentFixture<GonListComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ GonListComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(GonListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -8,10 +8,11 @@ import { PageNotFoundComponent } from './components/';
 import { WebviewDirective } from './directives/';
 import { FormsModule } from '@angular/forms';
 import { GonListItemComponent } from './components/gon-list-item/gon-list-item.component';
+import { GonListComponent } from './components/gon-list/gon-list.component';
 
 @NgModule({
-  declarations: [PageNotFoundComponent, WebviewDirective, GonListItemComponent, GonButtonComponent],
+  declarations: [PageNotFoundComponent, WebviewDirective, GonListItemComponent, GonButtonComponent,GonListComponent],
   imports: [CommonModule, TranslateModule, FormsModule],
-  exports: [TranslateModule, WebviewDirective, FormsModule, GonListItemComponent, GonButtonComponent]
+  exports: [TranslateModule, WebviewDirective, FormsModule, GonListItemComponent, GonButtonComponent,GonListComponent]
 })
 export class SharedModule { }
