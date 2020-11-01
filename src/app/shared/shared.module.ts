ff334e110bfd6d98d944786aba80ec629bcf9a82
Kevin <l8iunix8@gmail.com>
2020-11-01 15:26:09 +0800

    Kevin/GonList_UI
    新增共用元件GonList的介面

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,17 +1,31 @@
-import { GonButtonComponent } from './components/gon-button/gon-button.component';
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { GonButtonComponent } from "./components/gon-button/gon-button.component";
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { TranslateModule } from '@ngx-translate/core';
+import { TranslateModule } from "@ngx-translate/core";
 
-import { PageNotFoundComponent } from './components/';
-import { WebviewDirective } from './directives/';
-import { FormsModule } from '@angular/forms';
-import { GonListItemComponent } from './components/gon-list-item/gon-list-item.component';
+import { PageNotFoundComponent } from "./components/";
+import { WebviewDirective } from "./directives/";
+import { FormsModule } from "@angular/forms";
+import { GonListItemComponent } from "./components/gon-list-item/gon-list-item.component";
+import { GonListComponent } from "./components/gon-list/gon-list.component";
 
 @NgModule({
-  declarations: [PageNotFoundComponent, WebviewDirective, GonListItemComponent, GonButtonComponent],
+  declarations: [
+    PageNotFoundComponent,
+    WebviewDirective,
+    GonListItemComponent,
+    GonButtonComponent,
+    GonListComponent,
+  ],
   imports: [CommonModule, TranslateModule, FormsModule],
-  exports: [TranslateModule, WebviewDirective, FormsModule, GonListItemComponent, GonButtonComponent]
+  exports: [
+    TranslateModule,
+    WebviewDirective,
+    FormsModule,
+    GonListItemComponent,
+    GonButtonComponent,
+    GonListComponent,
+  ],
 })
-export class SharedModule { }
+export class SharedModule {}
