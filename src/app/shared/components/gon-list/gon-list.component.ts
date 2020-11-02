8a314d2b850a1f11bb54681575893aac7c27d18b
Kevin <l8iunix8@gmail.com>
2020-11-03 00:24:37 +0800

    Kevin/GonList_AddEvent
    新增事件處理

--- src/app/shared/components/gon-list/gon-list.component.ts
+++ src/app/shared/components/gon-list/gon-list.component.ts
@@ -1,58 +1,39 @@
-import { Component, OnInit, Input, OnChanges } from "@angular/core";
+import { Component, OnInit, Input, OnChanges, Output, EventEmitter } from "@angular/core";
 import { GonListData } from "./../../../difs/gon-list-data";
 @Component({
   selector: "app-gon-list",
   templateUrl: "./gon-list.component.html",
   styleUrls: ["./gon-list.component.scss"],
 })
-export class GonListComponent implements OnInit, OnChanges {
+export class GonListComponent implements  OnChanges {
   constructor() {}
   @Input() listData: GonListData[];
+  @Output() clickData = new EventEmitter<Object>();
 
   clickedObj = {};
-
-  gonListData = [
-    { name: "第一首音樂" },
-    { name: "第二首音樂" },
-    { name: "第三首音樂" },
-    { name: "第四首音樂" },
-    { name: "第五首音樂" },
-    { name: "第六首音樂" },
-    { name: "第七首音樂" },
-    { name: "第八首音樂" },
-    {
-      name:
-        "第123456789789123456789789123456789789123456789789123456789789123456789789123456789789123456789789123456789789123456789789123456789789123456789789123456789789123456789789首音樂",
-    },
-  ];
-
+  gonListData = [];
+  
+  //資料改成父元件傳入 所以用onChanges
   ngOnChanges(): void {
-    // this.gonListData = [];
-    // if (this.listData) {
-    // this.gonListData = this.listData;
+    this.gonListData = [];
+    if (this.listData) {
+    this.gonListData = this.listData;
 
     this.gonListData.forEach((data, index) => {
       this.clickedObj[`item_${index}`] = false;
     });
-    // }
+    }
   }
 
-  ngOnInit(): void {
-    // this.gonListData = [];
-    // if (this.listData) {
-    // this.gonListData = this.listData;
-
-    this.gonListData.forEach((data, index) => {
-      this.clickedObj[`item_${index}`] = false;
-    });
-    // }
-  }
-
-  listClick(index) {
+  listClick(index,data:GonListData) {
+    const obj = {};
+    obj[`index`] = index;
+    obj[`name`] = data.name
     const keys = Object.keys(this.clickedObj);
     keys.forEach((element) => {
       this.clickedObj[element] = false;
     });
     this.clickedObj[`item_${index}`] = true;
+    this.clickData.emit(obj);
   }
 }
